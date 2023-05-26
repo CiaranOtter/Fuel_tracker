482c4b5e4d79348e4d834e319d6e4cc3d04f18a2
ciaran <ciaranjotter@gmail.com>
2023-05-26 09:43:23 +0200

    reduced to good ol' fashioned html

--- fuel_tracker/src/routes/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-    import FuelBlock from "../components/fuel_block.svelte";
-    import InsertFuel from "../components/insert_fuel.svelte";
-</script>
-
-<InsertFuel />
-<FuelBlock />
---
+++ script.js
@@ -0,0 +1,24 @@
+const cost_in = document.getElementById('cost');
+const petrol_in = document.getElementById('petrol');
+const price_in = document.getElementById('price');
+const save_button = document.getElementById('save');
+
+const save_fuel = () => {
+    let amount = petrol_in.value;
+    let cost = cost_in.value;
+    let price = price_in.value;
+
+    let url = `http://otternonesenses.co.za/refuel-api/insert-fuel.php?cost=${cost}$price=${price}&liters=${amount};`
+
+    fetch(url)
+    .then(res => res.text())
+    .then(data => {
+        if (data == "1") {
+            console.log(data, "save was successful");
+        } else {
+            console.log(data, "save was unsuccessful");
+        }
+    })
+}
+
+save_button.addEventListener('click', save_fuel);
