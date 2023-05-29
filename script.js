a28ec7600c8e1b406d3668fea65e6517ad933188
ciaran <ciaranjotter@gmail.com>
2023-05-29 11:36:10 +0200

    updating delete api

--- script.js
+++ script.js
@@ -21,26 +21,67 @@ const save_fuel = () => {
         } else {
             console.log(data, "save was unsuccessful");
         }
-    })
+    });
 }
 
-const fetch_fuel = () => {
-    let url = `http://otternonesenses.co.za/Fuel_tracker/refuel-api/fetch_fuel.php`
-
+const remove_item = (obj) => {
+    console.log("delete this item", obj) 
+    
+    let url = "http://otternonesenses.co.za/Fuel_tracker/refuel-api/delete_fuel.php?id=" + obj['id'];
     fetch(url)
-    .then(res => res.json())
+    .then(res => res.text())
     .then(data => {
-        console.log(data);
-        for (let obj of data) {
-            format_fuel_block(obj);
+        if (data == "1") {
+            console.log("delete was succesful");
+        } else {
+            console.log("delete was unsuccessful");
         }
-    })
+    });
 }
+
+const show_menu = (e, obj) => {
+    console.log("show menu", obj, e);
+
+    let menu = document.createElement('div');
+    menu.classList.add('menu');
+    menu.setAttribute('position', 'absolute')
+    menu.setAttribute('top', e.clientY);
+    menu.setAttribute('left', e.clientX);
+    document.body.appendChild(menu);
+
+    let delete_item = document.createElement('div');
+    delete_item.classList.add('menu-item');
+    delete_item.innerHTML = "Delete";
+    delete_item.addEventListener('click', () => {
+        remove_item(obj)
+    });
+
+    menu.appendChild(delete_item);
+}
+
 
 const format_fuel_block = (obj) => {
     console.log(obj)
     let cont = document.createElement('div')
     cont.classList.add('fuel_block')
+
+    let inner_cont = document.createElement('div')
+    inner_cont.classList.add('flex_container')
+    inner_cont.classList.add('left');
+
+    let edit_cont = document.createElement('div')
+    edit_cont.classList.add('flex_container')
+    edit_cont.classList.add('right');
+
+    delete_icon = document.createElement('i');
+    delete_icon.classList.add('material-icons');
+    delete_icon.classList.add('delete-icons');
+    delete_icon.classList.add('icon');
+    delete_icon.innerHTML = "more_vert";
+    delete_icon.addEventListener('click', (e) => show_menu(e, obj));
+
+    // cont.appendChild(edit_cont);
+    edit_cont.appendChild(delete_icon);
 
 
     let title = document.createElement('div');
@@ -70,9 +111,13 @@ const format_fuel_block = (obj) => {
         value.innerHTML = obj[key];
         row.appendChild(value);
 
-        cont.appendChild(row);
+        inner_cont.appendChild(row);
 
     }
+
+    cont.appendChild(edit_cont);
+    cont.appendChild(inner_cont);
+    
 
     fuel_block.appendChild(cont);
 }
