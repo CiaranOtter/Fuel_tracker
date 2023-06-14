bd9e5be1be16af530cf4d4a1dc1f47fbe1435548
ciaran <ciaranjotter@gmail.com>
2023-06-14 12:00:49 +0200

    adding termianting line to add car

--- refuel-api/add_car.php
+++ refuel-api/add_car.php
@@ -11,11 +11,8 @@ if (isset($post['name'])) {
 
 
 $sql = "INSERT INTO car_profile (name) VALUES ('$car_name')";
-
-if ($car_name!= '') {
-    $output = array('success' => false);
-    echo json_encode($output);
-    exit;
+if ($car_name == '') {
+    die(json_encode(array('success' => false)));
 }
 if ($res = $conn->query($sql)) {
     $output = array('success' => true);
