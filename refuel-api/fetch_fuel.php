5bcf0c0b9560a22a07668613ebeb961fbf1e1aec
ciaran <ciaranjotter@gmail.com>
2023-06-16 14:59:05 +0200

    adding fetch fuel api

---
+++ refuel-api/fetch_fuel.php
@@ -0,0 +1,19 @@
+<?php
+
+include('conf.php');
+
+$sql = "SELECT * FROM reFuel";
+
+$output = array("success" => false, data => array());
+
+if ($res = $conn->query($sql)) {
+    $output['success'] = true;
+    while ($row = $res->fetch_assoc()) {
+        $output['data'][] = $row;
+    }
+}
+
+echo json_encode($output);
+
+
+?>
