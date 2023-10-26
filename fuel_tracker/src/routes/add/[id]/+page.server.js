56d678a66b28a833709f29bd0247ac63c89d7709
ciaran otter <ciaranjotter@gmail.com>
2023-10-26 11:06:58 +0200

    updated add fuel

--- fuel_tracker/src/routes/add/[id]/+page.server.js
+++ fuel_tracker/src/routes/add/[id]/+page.server.js
@@ -1,17 +1,18 @@
 
 
 export async function load({params}) {
-    let url = "http://otternonesenses.co.za/Fuel_tracker/refuel-api/fetch_car.php";
-    let res = await fetch(url, {
-        method: "POST",
-        body: JSON.stringify({
-            id: params.id
-        })
-    })
-
-    let data = await res.text();
-
     return {
-        car: data
+        car_id: params.id
+    }
+}
+
+export const actions = {
+    default: async ({request}) => {
+        const data = await request.formData();
+        const car_id = data.get('car_id');
+        const price = data.get('price');
+        const cost = data.get('cost');
+        const liters = data.get('liters');
+        
     }
 }
--- fuel_tracker/src/routes/add/[id]/+page.svelte
+++ fuel_tracker/src/routes/add/[id]/+page.svelte
@@ -1,4 +1,19 @@
 <script>
+    import { CarFuel } from '$classes/Cars_Fuel.js';
+
     export let data;
-    console.log(data);
+    let car_id = data.car_id;
 </script>
+
+<form method="post">
+    <input id="car_id_input" type="hidden" value="{car_id}" name="car_id" /><br/>
+    <label for="price_input">Current price of fuel:</label><br/>
+    <input id="price_input" type="number" name="price" value="0"/><br/>
+    <label for="cost_input">Amount spent on fuel:</label><br/>
+    <input id="cost_input" type="number" name="cost" value="0"/><br/>
+    <label for="liters_input">Liters purchased:</label><br/>
+    <input id="liters_input" type="number" name="liters" value="0" /><br/>
+    <label for="kmTraveled_input">Kms travled on car:</label><br/>
+    <input id="kmTraveled_input" type="number" name="kmTravled" value="0" /><br/>
+    <input type="submit" value="Add Fuel" />
+</form>
