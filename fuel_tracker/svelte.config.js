c21491d44d0dff76a4b90b973b821cdd8ca376be
Ciaran Otter <ciaranjotter@gmail.com>
2023-11-22 02:56:23 +0200

    update

--- fuel_tracker/svelte.config.js
+++ fuel_tracker/svelte.config.js
@@ -3,7 +3,13 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			pages: 'public',
+			assets: 'public',
+			fallback: undefined,
+			precompress: false, 
+			strict: false
+		}),
 		alias: {
 			$classes: './src/lib/classes',
 			$components: './src/lib/components',
